e545883ab4e53d53f7c5f38bc4a48fec8c895c47
service-01 <service@aol.com>
2024-10-23 15:36:31 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Primitive Types
+let arr = [1, "Hello", true, 7, false, undefined, null];
+arr[0] = 22;
+console.log(arr);
---
+++ app.ts
@@ -0,0 +1,16 @@
+// Primitive Types
+// null
+// number
+// undefined
+// boolean
+// string
+// any
+
+// Arrays
+let strArr: string[] = ['true', "james"];
+let numArr: boolean[][] = [[true, false], [true]];
+let tupleArr: [number, string] = [1, "x"]
+const coords: [number, number[]][] = [
+    [1, [3, 2]],
+    [-2, [93, 2]],
+]
